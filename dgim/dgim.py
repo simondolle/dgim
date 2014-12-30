e184526929667c3daeebe5c1bf72a67b613a0621
Simon Dollé <simon@botify.com>
2014-12-30 19:50:53 +0100

    First algorithm implementation.
    Naive algorithm implementation.
    Pass the basic unit tests.

--- dgim/dgim.py
+++ dgim/dgim.py
@@ -1,3 +1,5 @@
+import itertools
+
 class Dgim(object):
     """An implementation of the DGIM algorithm.
     It estimates the number of 1s present in a sliding window of a stream
@@ -17,6 +19,8 @@ class Dgim(object):
         :type N: int
         """
         self.N = N
+        self.buckets = []
+        self.timestamp = 0
 
     def update(self, elt):
         """Update the stream with one element.
@@ -24,14 +28,52 @@ class Dgim(object):
         :param elt: the latest element of the stream
         :type elt: int
         """
-        pass
+        #check if oldest bucket should be removed
+        if (len(self.buckets) > 0 and
+                self.buckets[-1].most_recent_timestamp < self.timestamp - self.N):
+            self.buckets = self.buckets[:-2]
+        if elt != 1:
+            self.timestamp += 1
+            return
+        reminder = Bucket(self.timestamp, 1)
+        new_buckets = []
+        for k, crt_buckets in itertools.groupby(self.buckets, key=lambda x: x.one_count):
+            if reminder is not None:
+                crt_buckets = [reminder] + list(crt_buckets)
+            else:
+                crt_buckets = list(crt_buckets)
+            if len(crt_buckets) <= 2:
+                reminder = None
+                new_buckets.extend(crt_buckets)
+            elif len(crt_buckets) == 3:
+                new_buckets.append(crt_buckets[0])
+                crt_buckets[1].merge(crt_buckets[2])
+                reminder = crt_buckets[1]
+            else:
+                raise ValueError("Too many elements")
+        if reminder is not None:
+            new_buckets.append(reminder)
+        self.buckets = new_buckets
+        self.timestamp += 1
 
     def get_count(self):
         """Returns an estimate of the number of ones in the sliding window.
         :returns: int
         """
-        pass
-
+        #find the all the buckets which most recent timestamp is ok
+        buckets = []
+        for bucket in self.buckets:
+            if bucket.most_recent_timestamp < self.timestamp - self.N:
+                break
+            buckets.append(bucket)
+        if len(buckets) == 0:
+            return 0
+        result = 0
+        last_bucket = buckets[-1]
+        for bucket in buckets[0:-1]:
+            result += bucket.one_count
+        result += (last_bucket.one_count)/2
+        return result
 
 class Bucket(object):
     """A class to represent a bucket."""
@@ -51,4 +93,10 @@ class Bucket(object):
         :param other_bucket: the bucket that has to be merged with this one.
         :type other_bucket: Bucket
         """
-        pass
+        self.most_recent_timestamp = max(
+                self.most_recent_timestamp,
+                other_bucket.most_recent_timestamp)
+        self.one_count += other_bucket.one_count
+
+    def __repr__(self):
+        return "Bucket {}: {}".format(self.most_recent_timestamp, self.one_count)
